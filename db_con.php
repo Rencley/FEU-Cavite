448d9dd0001fa89924f3c9812dd537e48afa3296
Rencley <58833814+Rencley@users.noreply.github.com>
2019-12-14 15:45:08 +0800

    Update db_con.php

--- db_con.php
+++ db_con.php
@@ -1,10 +1,10 @@
 <?php
 
 // Connection variables 
-$host = "localhost"; // MySQL host name eg. localhost
-$user = "root"; // MySQL user. eg. root ( if your on localserver)
-$password = ""; // MySQL user password  (if password is not set for your root user then keep it empty )
-$database = "feuc_db"; // MySQL Database name
+$host = "sql12.freesqldatabase.com"; // MySQL host name eg. localhost
+$user = "sql12315650"; // MySQL user. eg. root ( if your on localserver)
+$password = "9MblzzbpSN"; // MySQL user password  (if password is not set for your root user then keep it empty )
+$database = "sql12315650"; // MySQL Database name
 
 // Connect to MySQL Database 
 $db = mysqli_connect($host, $user, $password) or die("Could not connect to database");
